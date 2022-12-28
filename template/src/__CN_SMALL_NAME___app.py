ed2d2495bffd1f00798d64c8dfbaa3d4dc380458
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2022-12-28 18:03:25 -0500

    changed version string stuff

--- template/src/__CN_SMALL_NAME___app.py
+++ template/src/__CN_SMALL_NAME___app.py
@@ -25,9 +25,25 @@
 
 
 # ------------------------------------------------------------------------------
-# Short description
+# The main function of the program
 # ------------------------------------------------------------------------------
 def main():
+
+    """
+        The main function of the program
+
+        This function is the main entry point for the program, initializing the
+        program. and performing it's steps.
+    """
+
+    # call the steps in order
+    func()
+
+
+# ------------------------------------------------------------------------------
+# Short description
+# ------------------------------------------------------------------------------
+def func():
 
     """
         Short description
@@ -45,13 +61,21 @@ def main():
         Long description
     """
 
-    pass
+    return ('this is a test')
 
 
 # ------------------------------------------------------------------------------
 # Code to run when called from command line
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
+
+    """
+        Code to run when called from command line
+
+        This is the top level code of the program, called when the Python file
+        is invoked from the command line, e.g. "python __CN_SMALL_NAME__.py".
+    """
+
     main()
 
 # -)
