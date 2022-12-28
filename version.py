d5ca2c61cefe7730ed551e03802b4f7067e1bfb2
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2022-12-28 17:56:43 -0500

    added get_version func

--- version.py
+++ version.py
@@ -12,4 +12,21 @@
 
 __version__ = '0.1.0'
 
+
+# ------------------------------------------------------------------------------
+# Returns the canonical version number of this project
+# ------------------------------------------------------------------------------
+def get_version():
+
+    """
+        Returns the canonical version number of this project
+
+        This is the canonical (only and absolute) version number string for this
+        project. This sjould provide the absolute version number string (in
+        semantic notation) of this project, and all other version numbers should
+        be superceded by this string.
+    """
+
+    return __version__
+
 # -)
