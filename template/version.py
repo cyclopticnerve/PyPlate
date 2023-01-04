b51e235b765c9bfbba1fcd0943bef0114e51ff48
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2023-01-04 14:09:23 -0500

    removed get_version func

--- template/version.py
+++ template/version.py
@@ -10,27 +10,10 @@
 # Constants
 # ------------------------------------------------------------------------------
 
+# This is the canonical (only and absolute) version number string for this
+# project. This should provide the absolute version number string (in
+# semantic notation) of this project, and all other version numbers should
+# be superceded by this string.
 __version__ = '__CN_VERSION__'
-
-# ------------------------------------------------------------------------------
-# Functions
-# ------------------------------------------------------------------------------
-
-
-# ------------------------------------------------------------------------------
-# Returns the canonical version number of this project
-# ------------------------------------------------------------------------------
-def get_version():
-
-    """
-        Returns the canonical version number of this project
-
-        This is the canonical (only and absolute) version number string for this
-        project. This should provide the absolute version number string (in
-        semantic notation) of this project, and all other version numbers should
-        be superceded by this string.
-    """
-
-    return __version__
 
 # -)
