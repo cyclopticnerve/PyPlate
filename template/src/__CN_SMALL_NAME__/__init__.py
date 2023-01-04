22d132cf216c045ed36273381ffc85c9a40d1d4b
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2023-01-04 14:10:23 -0500

    cleaned up file

--- template/src/__CN_SMALL_NAME__/__init__.py
+++ template/src/__CN_SMALL_NAME__/__init__.py
@@ -7,26 +7,22 @@
 # ------------------------------------------------------------------------------
 
 # this file is only for package projects (multiple modules)
+# put any code here that you want to run whenever this package or its modules
+# are imported in a project
 
-"""
-    __CN_SHORT_DESC__
-
-    Modules:
-        ...
-"""
-
-__version__ = '__CN_VERSION__'
-
-print('this is the (pkg) __CN_SMALL_NAME__/__init__.py file')
+# ------------------------------------------------------------------------------
+# Imports
+# ------------------------------------------------------------------------------
 
 # import all modules in package to get full dot notation from package import
 # from <package> imoport <module>
 # TODO: fix these names
-from __CN_SMALL_NAME__ import mod_name_1 # noqa W0611 (unused import)
-from __CN_SMALL_NAME__ import mod_name_2 # noqa W0611 (unused import)
+from __CN_SMALL_NAME__ import * # noqa W0611 (unused import)
+# from __CN_SMALL_NAME__ import mod_name_1 # noqa W0611 (unused import)
+# from __CN_SMALL_NAME__ import mod_name_2 # noqa W0611 (unused import)
 
 # from <package_name> import *
 # TODO: fix these names
-__all__ = ['mod_name_1', 'mod_name_2']  # ['module_name', ...]
+# __all__ = ['mod_name_1', 'mod_name_2']  # ['module_name', ...]
 
 # -)
